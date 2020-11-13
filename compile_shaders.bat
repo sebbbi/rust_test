bab49d4d77b54e7df5250a5539a3bbe4d606e8d2
Sebastian Aaltonen <sebastian.aaltonen@unity3d.com>
2020-11-13 14:00:02 +0200

    depth pyramid shaders

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,4 +1,8 @@
 glslc.exe shader/main.vert -o shader/main_vert.spv
 glslc.exe shader/main.frag -o shader/main_frag.spv
 glslc.exe shader/main_frontface.vert -o shader/main_frontface_vert.spv
+
 glslc.exe shader/simple.frag -o shader/simple_frag.spv
+
+glslc.exe shader/depth_pyramid_first_mip.comp -o shader/depth_pyramid_first_mip.spv
+glslc.exe shader/depth_pyramid_downsample.comp -o shader/depth_pyramid_downsample.spv
