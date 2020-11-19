f9683ebde8743b7b5b1fde3f622f21559d28484c
Sebastian Aaltonen <sebastian.aaltonen@unity3d.com>
2020-11-19 18:33:05 +0200

    depth pyramid mipgen single pass version (coherent memory)

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -6,3 +6,4 @@ glslc.exe shader/simple.frag -o shader/simple_frag.spv
 
 glslc.exe shader/depth_pyramid_first_mip.comp -o shader/depth_pyramid_first_mip.spv
 glslc.exe shader/depth_pyramid_downsample.comp -o shader/depth_pyramid_downsample.spv
+glslc.exe shader/depth_pyramid_downsample_all.comp -o shader/depth_pyramid_downsample_all.spv
